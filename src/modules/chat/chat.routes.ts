04d883b40467290fa6d8acd398d12f9d908fa0e1
shubhampatel9977 <shubhampatel99260@gmail.com>
2026-06-14 03:13:15 +0530

    add streaming for user response

--- src/modules/chat/chat.routes.ts
+++ src/modules/chat/chat.routes.ts
@@ -3,11 +3,14 @@ import { Router } from "express";
 import { authorize } from "@middlewares/auth.middleware";
 
 import { sendMessageController } from "./chat.controller";
+import { sendMessageStreamController } from "./chat-stream.controller";
 
 const router = Router();
 
 router.use(authorize());
 
 router.post("/send", sendMessageController);
+
+router.post("/stream", sendMessageStreamController);
 
 export default router;
