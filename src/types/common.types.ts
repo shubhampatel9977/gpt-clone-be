b7323e1f6f8a8fcb2d40e93df04c5129420b2b13
shubhampatel9977 <shubhampatel99260@gmail.com>
2026-06-15 14:00:47 +0530

    fix some bugs related to type

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -1,3 +1,11 @@
+export type UserRole = "USER" | "ADMIN";
+
+export interface JwtPayload {
+  userId: string;
+  email: string;
+  role: UserRole;
+}
+
 export type ChatRole =
   | "user"
   | "assistant"
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,5 +1,5 @@
 import "express-serve-static-core";
-import { JwtPayload } from "./auth.types";
+import { JwtPayload } from "@app-types/common.types";
 
 declare module "express-serve-static-core" {
   interface Request {
